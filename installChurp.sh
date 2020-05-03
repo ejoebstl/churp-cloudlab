fa5573df11850ffa212d80708722c5f03351839c
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2020-05-03 22:38:01 +0200

    Moved installChurp file.

--- installChurp.sh
+++ installChurp.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Base dependencies
-sudo apt-get install gmp-dev flex bison git go
+sudo apt-get update
+sudo apt-get install -y libgmp-dev flex bison
 
+# Install GO. We need at lest version 1.14
+curl -LO https://get.golang.org/$(uname)/go_installer && chmod +x go_installer && ./go_installer && rm go_installer
+source /root/.bash_profile
+
+# Install pbc dependency
 wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
 tar xvzf pbc-0.5.14.tar.gz
 cd pbc-0.5.14
@@ -10,11 +16,9 @@ cd pbc-0.5.14
 make
 sudo make install
 cd ..
-rm pbc-0.5.14.tar.gz
-rm -rf pbc-0.5.14
 
 # CHURP
-git clone git@github.com:CHURPTeam/CHURP.git
+git clone https://github.com/CHURPTeam/CHURP.git
 
 cd CHURP/src/
 
