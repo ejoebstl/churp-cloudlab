59033f5ca5d05aec8e03509b2cf537349dffa2ad
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2020-05-03 21:20:22 +0200

    Changed back to proxy script.

--- profile.py
+++ profile.py
@@ -19,7 +19,8 @@ network = request.LAN("network0")
 # Helpers for configuring each docker container
 def requestContainer(name):
     node = request.DockerContainer(name)
-    node.docker_extimage = 'ejoebstl/churp-cloudlab'
+    #node.docker_extimage = 'ejoebstl/churp-cloudlab'
+    node.docker_dockerfile = 'https://raw.githubusercontent.com/ejoebstl/churp-cloudlab/master/Dockerfile.proxy'
     iface = node.addInterface("if1")
     network.addInterface(iface)
     return node
