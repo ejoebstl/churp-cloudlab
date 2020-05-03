ac905a4b41136a996c75e7dbfe0edf3fa6acf3b7
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2020-05-03 21:16:50 +0200

    Added extimage again.

--- profile.py
+++ profile.py
@@ -19,7 +19,7 @@ network = request.LAN("network0")
 # Helpers for configuring each docker container
 def requestContainer(name):
     node = request.DockerContainer(name)
-    node.docker_dockerfile = 'https://raw.githubusercontent.com/ejoebstl/churp-cloudlab/master/Dockerfile.proxy'
+    node.docker_extimage = 'ejoebstl/churp-cloudlab'
     iface = node.addInterface("if1")
     network.addInterface(iface)
     return node
