e233a0f05d7e79323b07430ca507f4841f4bf5d8
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2020-05-03 20:28:38 +0200

    Use fully qualified path to docker registry.

--- profile.py
+++ profile.py
@@ -19,7 +19,7 @@ network = request.LAN("network0")
 # Helpers for configuring each docker container
 def requestContainer(name):
     node = request.DockerContainer(name)
-    node.docker_extimage = 'ejoebstl/churp-cloudlab'
+    node.docker_extimage = 'registry.hub.docker.com/ejoebstl/churp-cloudlab'
     iface = node.addInterface("if1")
     network.addInterface(iface)
     return node
